f6a9a070a224561a6bd609faa531ad44a023e675
Jonathan Pauluze <jonathanpauluze@gmail.com>
2021-01-28 02:12:25 -0300

    feat: create Quiz page

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,42 +1,77 @@
-import Head from 'next/head';
+import { useState, useEffect } from 'react';
 
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
-import QuizQuestion from '../src/components/QuizQuestion';
+import QuizContainer from '../src/components/QuizContainer';
+import QuestionWidget from '../src/components/QuestionWidget';
 
 import db from '../db.json';
 
-const questions = db.questions;
+const { questions } = db;
 
-export default function Quiz() {
+const screenStates = {
+  LOADING: 'LOADING',
+  QUIZ: 'QUIZ',
+  RESULT: 'RESULT',
+};
+
+function LoadingWidget() {
   return (
-    <>
-      <Head>
-        <title>Valorant Quiz</title>
-      </Head>
+    <Widget>
+      <Widget.Header>
+        Carregando...
+      </Widget.Header>
 
-      <QuizBackground backgroundImage={db.bg}>
-        <QuizQuestion>
-          {questions.map(({id, image, title, description, alternatives, answer}) => (
-            <QuizQuestion.Card key={id}>
-              <QuizQuestion.Image src={image} />
+      <Widget.Content>
+        ...
+      </Widget.Content>
+    </Widget>
+  );
+}
 
-              <QuizQuestion.Content>
-                <QuizQuestion.Title>{title}</QuizQuestion.Title>
+export default function QuizPage() {
+  const [screenState, setScreenState] = useState(screenStates.LOADING);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const totalQuestions = questions.length;
+  const questionIndex = currentQuestion;
+  const question = questions[questionIndex];
 
-                <QuizQuestion.Description>{description}</QuizQuestion.Description>
+  useEffect(() => {
+    setTimeout(() => {
+      setScreenState(screenStates.QUIZ);
+    }, 1000);
+  }, []);
 
-                <QuizQuestion.Alternatives>
-                  {alternatives.map((alternative, index) => (
-                    <QuizQuestion.Alternative key={index}>
-                      {alternative}
-                    </QuizQuestion.Alternative>
-                  ))}
-                </QuizQuestion.Alternatives>
-              </QuizQuestion.Content>
-            </QuizQuestion.Card>
-          ))}
-        </QuizQuestion>
-      </QuizBackground>
-    </>
+  function handleQuizSubmit() {
+    const nextQuestion = questionIndex + 1;
+
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setScreenState(screenStates.RESULT);
+    }
+  }
+
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        {screenState === screenStates.LOADING && <LoadingWidget />}
+
+        {screenState === screenStates.QUIZ && (
+          <QuestionWidget
+            question={question}
+            questionIndex={questionIndex}
+            totalQuestions={totalQuestions}
+            onSubmit={handleQuizSubmit}
+          />
+        )}
+
+        {screenState === screenStates.RESULT && (
+          <h1>Parabains acertaste tudo</h1>
+        )}
+      </QuizContainer>
+    </QuizBackground>
   );
 }
