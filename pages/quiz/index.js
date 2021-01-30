a0d1bfc7145169152ac161f95d23babdf50d2d10
Jonathan Pauluze <jonathanpauluze@gmail.com>
2021-01-30 02:06:51 -0300

    feat: decides if render external or internal db

--- pages/quiz/index.js
+++ pages/quiz/index.js
@@ -1,114 +1,54 @@
-import { useState, useEffect } from 'react';
+import { ThemeProvider } from 'styled-components';
 import PropTypes from 'prop-types';
+import QuizPage from '../../src/screens/quiz';
 
-import Widget from '../../src/components/Widget';
-import QuizLogo from '../../src/components/QuizLogo';
-import QuizBackground from '../../src/components/QuizBackground';
-import QuizContainer from '../../src/components/QuizContainer';
-import QuestionWidget from '../../src/components/QuestionWidget';
+export default function QuizScreen({ db }) {
+  if (db) {
+    return (
+      <ThemeProvider theme={db.theme}>
+        <QuizPage db={db} />
+      </ThemeProvider>
+    );
+  }
 
-import db from '../../db.json';
+  return (
+    <QuizPage db={db} />
+  );
+}
 
-const { questions } = db;
+export async function getServerSideProps(context) {
+  const { external } = context.query;
 
-const screenStates = {
-  LOADING: 'LOADING',
-  QUIZ: 'QUIZ',
-  RESULT: 'RESULT',
+  if (!external) {
+    return {
+      props: {},
+    };
+  }
+
+  const db = await fetch(`${external}/api/db`)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      throw new Error('Falha ao buscar dados.');
+    })
+    .then((db) => db)
+    .catch((err) => {
+      console.log(err);
+    });
+
+  return {
+    props: {
+      db,
+    },
+  };
+}
+
+QuizScreen.defaultTypes = {
+  db: undefined,
 };
 
-function LoadingWidget() {
-  return (
-    <Widget>
-      <Widget.Header>
-        Carregando...
-      </Widget.Header>
-
-      <Widget.Content>
-        ...
-      </Widget.Content>
-    </Widget>
-  );
-}
-
-function ResultWidget({ results }) {
-  return (
-    <Widget>
-      <Widget.Header>
-        Resultado
-      </Widget.Header>
-
-      <Widget.Content>
-        <p>
-          Você acertou
-          {' '}
-          {results.filter((result) => result).length}
-        </p>
-
-        <ul>
-          {results.map((result, index) => (
-            <li key={`result__${index + 1}`}>
-              {`#${index + 1} pergunta: ${result ? 'Acertou' : 'Errou'}`}
-            </li>
-          ), 0)}
-        </ul>
-      </Widget.Content>
-    </Widget>
-  );
-}
-
-export default function QuizPage() {
-  const [screenState, setScreenState] = useState(screenStates.LOADING);
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [results, setResults] = useState([]);
-  const totalQuestions = questions.length;
-  const questionIndex = currentQuestion;
-  const question = questions[questionIndex];
-
-  useEffect(() => {
-    setTimeout(() => {
-      setScreenState(screenStates.QUIZ);
-    }, 1000);
-  }, []);
-
-  function handleQuizSubmit() {
-    const nextQuestion = questionIndex + 1;
-
-    if (nextQuestion < questions.length) {
-      setCurrentQuestion(nextQuestion);
-    } else {
-      setScreenState(screenStates.RESULT);
-    }
-  }
-
-  function addResult(result) {
-    setResults([...results, result]);
-  }
-
-  return (
-    <QuizBackground backgroundImage={db.bg}>
-      <QuizContainer>
-        <QuizLogo />
-        {screenState === screenStates.LOADING && <LoadingWidget />}
-
-        {screenState === screenStates.QUIZ && (
-          <QuestionWidget
-            question={question}
-            questionIndex={questionIndex}
-            totalQuestions={totalQuestions}
-            onSubmit={handleQuizSubmit}
-            addResult={addResult}
-          />
-        )}
-
-        {screenState === screenStates.RESULT && (
-          <ResultWidget results={results} />
-        )}
-      </QuizContainer>
-    </QuizBackground>
-  );
-}
-
-ResultWidget.propTypes = {
-  results: PropTypes.arrayOf(PropTypes.bool).isRequired,
+QuizScreen.propTypes = {
+  db: PropTypes.object,
 };
